99df7b206536e0427d79041d7030f2f1f5ffba24
Savanah Russ <savanah.russ@yale.edu>
2018-03-06 12:01:26 -0500

    Hi Alyssa,
    This is my homework 6 question 3 issue. Please refer to the email for where the error is occurring. Thank you!

--- Homework6Q1.py
+++ Homework6Q1.py
@@ -37,10 +37,9 @@ class SetOfGames:
         self._value = self._gameRewards
         self._maxi = 0
         self._mini = 0
-        self._losslist=[]
+        self._losslist = []
 
-
-
+    def simulation(self, n_games,prob_head):
         for n in range(n_games):
             # create a new game
             game = Game(id=n, prob_head=prob_head)
@@ -50,60 +49,67 @@ class SetOfGames:
 
             self._gameRewards.append(game.get_reward())
 
-        self._sumStat_expectedrewards = Stat.SummaryStat('Game Reward', self._gameRewards)
-        self._sumStat_probabilityloss = Stat.SummaryStat('Probability of Loss', self._losslist)
+        for value in self._gameRewards:
+            if value < 0:
+                self._lose_count+=1
+                i=1
+                self._losslist.append(i)
+            elif value > 0:
+                i=0
+                self._losslist.append(i)
+        return SetOfGamesOutcomes(self)
+
 
     def get_game_rewards(self):
         return self._gameRewards
 
-    def get_loss_count(self):
-        for value in self._gameRewards:
-            if value < 0:
-                self._lose_count+=1
-        return self._lose_count / len(self._gameRewards)
-
     def get_loss_list(self):
-        for value in self._gameRewards:
-            if value < 0:
-                self._losslist.append(game.get_loss_count)
-
         return self._losslist
 
     def get_ave_reward(self):
         """ returns the average reward from all games"""
         return sum(self._gameRewards) / len(self._gameRewards)
 
-    def get_maximum(self):
-        for value in self._gameRewards:
-            if value>self._maxi:
-                self._maxi = value
-        return self._maxi
+    #def get_maximum(self):
+    #    for value in self._gameRewards:
+    #        if value>self._maxi:
+    #            self._maxi = value
+    #    return self._maxi
 
-    def get_minimum(self):
-        for value in self._gameRewards:
-            if value<self._mini:
-                self._mini = value
-        return self._mini
+    #def get_minimum(self):
+    #    for value in self._gameRewards:
+    #        if value<self._mini:
+    #            self._mini = value
+    #    return self._mini
 
-    def get_CI_game_reward(self,alpha):
+
+class SetOfGamesOutcomes:
+    def __init__(self, simulated_cohort):
+
+        self._simCohort = simulated_cohort
+        self._sumStat_expectedrewards = Stat.SummaryStat('Game Reward', self._simCohort.get_game_rewards())
+        self._sumStat_probabilityloss = Stat.SummaryStat('Probability of Loss', self._simCohort.get_loss_list())
+
+
+    def get_CI_reward(self, alpha):
         return self._sumStat_expectedrewards.get_t_CI(alpha)
 
-    def get_CI_loss_probability(self,alpha):
-        return self._sumStat_probabilityloss.get_t_CI(alpha)
 
+    def get_CI_loss(self,alpha):
+        return self._sumStat_probabilityloss.get_t_CI(alpha)
 
 
 # run trail of 1000 games to calculate expected reward
 games = SetOfGames(prob_head=0.5, n_games=1000)
-
+hw6trial = games.simulation(n_games=1000, prob_head=0.5)
 
 # print the average reward
-print('Expected reward when the probability of head is 0.5:', games.get_ave_reward())
-print('The maximum value of rewards is:', games.get_maximum())
-print('The minimum value of rewards is:', games.get_minimum())
-print('The probability of losing is:', games.get_loss_count())
-#print(games.get_loss_list())
+#print('Expected reward when the probability of head is 0.5:', games.get_ave_reward())
+#print('The maximum value of rewards is:', games.get_maximum())
+#print('The minimum value of rewards is:', games.get_minimum())
+#print('The probability of losing is:', games.get_loss_count())
 
-print('The 95% Confidence Interval for expected game rewards is:', games.get_CI_game_reward(alpha=0.05))
-print('The 95% Confidence Interval for probability loss is:', get_CI_loss_probability(alpha=0.05))
+
+print('The 95% Confidence Interval for expected game rewards is:', hw6trial.get_CI_reward(alpha=0.05))
+print('The 95% Confidence Interval for probability loss is:', hw6trial.get_CI_loss(alpha=0.05))
 
